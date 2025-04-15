79df8a50ea9156bec6dd5a280a4bb826a300c648
rouabhi nihal/sbai eya <nihal-nour-elhouda.rouabhi@etu.univ-amu.fr>
2025-04-15 03:21:04 +0200

    Résolution du conflit dans club/urls.py

--- club/urls.py
+++ club/urls.py
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 36545bc832983f4c6bf3d7723a6a96910bae9619
 from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.accueil, name='accueil'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/statut/', views.statut, name='statut'),
     path('dashboard/inscription/', views.inscription, name='inscription'),
     path('dashboard/concours/', views.concours, name='concours'),
     path('dashboard/chevaux/', views.chevaux, name='chevaux'),
+    path('dashboard/inscription-cavalier/', views.inscription_cavalier, name='inscription_cavalier'),
 ]
-
-=======
-    path('inscription/', views.inscription_cavalier, name='inscription_cavalier'),
-]
->>>>>>> 36545bc832983f4c6bf3d7723a6a96910bae9619
